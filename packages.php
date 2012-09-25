e66684c9b824b7102c6878fef7ad442028cbf96b
Michał Kluszewski <lupatus@lupatus.com>
2012-09-26 01:36:00 +0200

    fixed js basePaths

--- packages.php
+++ packages.php
@@ -5,18 +5,18 @@ return array(
 	'jquery-noconflict' => array(
 		'js' => array('jquery-noconflict.js'),
 		'depends' => array('jquery'),
-		'basePath' => 'ext.moo.assets',
+		'basePath' => 'ext.yii-moo.assets',
 	),
 	
 	'moo' => array(
 		'js' => array(YII_DEBUG ? 'mootools-core-1.4.5.js' : 'mootools-core-1.4.5-min.js'),
-		'basePath' => 'ext.moo.assets',
+		'basePath' => 'ext.yii-moo.assets',
 	),
 	
 	'moo-more' => array(
 		'depends' => array('moo'),
 		'js' => array(YII_DEBUG ? 'mootools-more-1.4.0.1.js' : 'mootools-more-1.4.5-min.js'),
-		'basePath' => 'ext.moo.assets',
+		'basePath' => 'ext.yii-moo.assets',
 	)
 	
 );
